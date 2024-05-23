d0c2b8947dcf3c4e77530e6a0ce4aea69a1f1f45
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2024-05-22 21:38:49 -0300

    Update main.js
    Logic complete only need the way of load data from the JSON files by URL to the file

--- main.js
+++ main.js
@@ -3,9 +3,46 @@ function showPosts(index)
 {
    // Load posts from JSON files
     let data =   {
-                   1 : "Research get data from a JSON file"
+                   "name" : "Research get data from a JSON file",
+                   "author" : "",
+                   "date" : "22/05/2024",
+                   "content" : ["text_1", "text_2"],
+                   "contacts" : [],
+                   "tags" : [1, 2, 3],
+                   "template" : "default.css"
                  };
+   // Summarize the content of the post
+   
+   let content = ('<br /><style src = "' + data["template" ]+ '" type = "text/css"></style><section id = "post-" ' + index + '>');
+   
+   for (text in data["content"])
+   {
+      content += ("<p>" + text + "</p>");
+   }
+  
+   contetnt += "</section><br />"
+   // List of tags
+   let tags = '<h2>Categories:</h2><br /><ul id = "tags">';
+
+   for (item in data[tags])
+   {
+      tags += ('<li>' + item + '</li>');
+   }
+
+   tags += "</ul><br />";
+   // List of E-mail contacts
+   let contacts = '<h3>Contacts:</h3><br /><ol id = "contacts">';
+
+   for (contact in data["contacts"])
+   {
+      contacts += ('<li><a href = "mailto:' + contact + '"</a>' + contact + '</li>');
+   }
+
+   contacts += "</ul><br />";
   // Render the post in HTML
-    let view = document.querySelector("#post-view");
-    view.innerHTML += ("<h1>${data[1]}</h1>");
+   if (index in data)
+   {   
+       let view = document.querySelector("#post-view");
+       view.innerHTML += ("<h1><u>" + data["name"] + "</u></h1><sup> " + data["author"] + " </sup><sub>" + data["date"] + "</sub>" + content + tags + contact);
+   }   
 }
